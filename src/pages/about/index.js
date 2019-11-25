cf12570d2b6bee8f6b8dd9174437863dfa38b3bf
Alexey Tsitskun <alexey.tsitskun@yandex.ru>
2019-11-25 21:44:15 +0300

    Going to install Slider

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -7,6 +7,7 @@ import Card from '../../blocks/common/card/card'
 import { loginForm, signupForm, regCompleteForm } from '../../blocks/common/auth-form/auth-form'
 
 // Methods
+
 window.onresize = () => {
   if (window.innerWidth > 767) mainMenu.close()
 }

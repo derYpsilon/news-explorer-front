7c9e466caef6af952f3abba9607704f26613a96d
Alexey Tsitskun <alexey.tsitskun@yandex.ru>
2019-12-01 20:49:09 +0300

    Login/Logout drafts are ready

--- src/components/api-explorer.js
+++ src/components/api-explorer.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable class-methods-use-this */
 import AuthForm from '../blocks/common/auth-form/auth-form'
+import config from './config'
 
 const loginForm = new AuthForm(
   document.querySelector('#login-form'),
@@ -27,8 +28,6 @@ class Explorer {
 
   // eslint-disable-next-line class-methods-use-this
   get userName() {
-    // return 'Иннокентий'
-
     return localStorage.getItem('user')
   }
 
@@ -37,22 +36,77 @@ class Explorer {
     if (!this.userName) {
       shownName.insertAdjacentText('afterbegin', 'Авторизуйтесь')
       shownName.addEventListener('click', () => loginForm.open())
-      shownName.querySelector('.menu__logout').style.display = 'none'
+      shownName.parentNode.querySelector('.menu__logout').style.display = 'none'
       document.querySelector('#menu-saved-articles').style.display = 'none'
     } else {
-      shownName.insertAdjacentText('afterbegin', this.userName)
-      shownName.querySelector('.menu__logout').style.display = 'inline-block'
+      shownName.textContent = this.userName
+      shownName.parentNode.querySelector('.menu__logout').style.display = 'inline-block'
       shownName.removeEventListener('click', () => loginForm.open())
+      shownName.addEventListener('click', () => this.logout())
       document.querySelector('#menu-saved-articles').style.display = 'flex'
     }
   }
 
+  logout() {
+    fetch(config.logout,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        mode: 'cors',
+        credentials: 'include',
+      })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Ошибка выхода ${res.status}`)
+        return res.json()
+      })
+      .then(() => {
+        localStorage.clear()
+        document.location.href = document.location.pathname === '/articles/' ? '../' : './'
+      })
+      .catch((e) => console.log(e.message))
+  }
+
   login(data) {
     console.log(data)
+    fetch(config.login,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        mode: 'cors',
+        credentials: 'include',
+        body: JSON.stringify({
+          password: '123456789',
+          email: 'user@user.ru',
+        }),
+      })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Ошибка авторизации ${res.status}`)
+        return res.json()
+      })
+      .then((answer) => {
+        console.log(answer)
+        fetch('https://api.tsitskun.tk/users/me', { credentials: 'include' })
+          .then((res) => {
+            if (!res.ok) throw new Error(`Ошибка чтения ${res.status}`)
+            return res.json()
+          })
+          .then((userInfo) => {
+            localStorage.setItem('user', userInfo.user)
+            this.menuCustomizer()
+          })
+          .catch((e) => console.log(e.message))
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
   }
 }
 
 const apiEx = new Explorer()
-loginForm.callExt = apiEx.login
+loginForm.callExt = apiEx.login.bind(apiEx)
 
 export default apiEx
---
+++ src/components/config.js
@@ -0,0 +1,8 @@
+const config = {
+  url: 'https://api.tsitskun.tk',
+  login: 'https://api.tsitskun.tk/signin',
+  signup: '',
+  logout: 'https://api.tsitskun.tk/logout',
+}
+
+export default config
